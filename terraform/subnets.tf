4999a0fd8dd6576b30c769d80380ca066c1595e8
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-02-05 16:13:23 -0500

    add subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -3,6 +3,10 @@ resource "aws_subnet" "public_1" {
     cidr_block = var.public_subnets[0]
     availability_zone = var.availability_zones[0]
     map_public_ip_on_launch = true
+
+    tag = {
+        Name = "${var.app_name}-public-1"
+    }
 }
 
 resource "aws_subnet" "public_2" {
@@ -10,6 +14,9 @@ resource "aws_subnet" "public_2" {
     cidr_block = var.public_subnets[1]
     availability_zone = var.availability_zones[1]
     map_public_ip_on_launch = true
+    tag = {
+        Name = "${var.app_name}-public-2"
+    }
 }
 
 resource "aws_subnet" "private_1" {
@@ -17,6 +24,9 @@ resource "aws_subnet" "private_1" {
     cidr_block = var.private_subnets[0]
     availability_zone = var.availability_zones[0]
     map_public_ip_on_launch = true
+    tag = {
+        Name = "${var.app_name}-private-1"
+    }
 }
 
 resource "aws_subnet" "private_2" {
@@ -24,4 +34,7 @@ resource "aws_subnet" "private_2" {
     cidr_block = var.private_subnets[1]
     availability_zone = var.availability_zones[1]
     map_public_ip_on_launch = true
+    tag = {
+        Name = "${var.app_name}-private-2"
+    }
 }
